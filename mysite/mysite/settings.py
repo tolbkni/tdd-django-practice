7b98903a5e64f6892ee8513f9b2ec2e9485e3c66
Tolbkni Kao <tolbkni@gmail.com>
2014-03-28 00:02:46 +0800

    Creates "polls" app, makes it to be active

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'fts',
+    'polls',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
