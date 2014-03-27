ce1b0734984bcb7ce7efc26e4a1e2f97e12c8246
Tolbkni Kao <tolbkni@gmail.com>
2014-03-27 23:30:23 +0800

    添加第一个测试用例

--- mysite/fts/tests.py
+++ mysite/fts/tests.py
@@ -5,12 +5,26 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class PollsTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_create_new_poll_via_admin_site(self):
+        # Open browser, and go to the admin page
+        self.browser.get(self.live_server_url + '/admin/')
+
+        # Find 'Django administration' in body.text
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)
+
+        # TODO use the admin site to create a Poll
+        self.fail('finish this test')
