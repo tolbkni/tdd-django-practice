79ad53ae725b0c8b0553d9440fda0605f8e1caf5
Tolbkni Kao <tolbkni@gmail.com>
2014-03-28 00:11:43 +0800

    First unit tests, testing a new "Poll" model

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -6,11 +6,26 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.utils import timezone
+from polls.models import Poll
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class PollModelTest(TestCase):
+
+    def test_creating_a_new_poll_and_saving_it_to_the_database(self):
+        poll = Poll()
+        poll.question = "What's up?"
+        poll.pub_date = timezone.now()
+
+        # check if we can save it to the database
+        poll.save()
+
+        # check we can find it in the database again
+        all_polls_in_database = Poll.objects.all()
+        self.assertEquals(len(all_polls_in_database), 1)
+        only_poll_in_database = all_polls_in_database[0]
+        self.assertEquals(only_poll_in_database, poll)
+
+        # check that it's saved question and pub_date
+        self.assertEquals(only_poll_in_database.question, "What's up?")
+        self.assertEquals(only_poll_in_database.pub_date, poll.pub_date)
