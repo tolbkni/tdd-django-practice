469624c0588b28b4cda37da3038fb67089783b12
Tolbkni Kao <tolbkni@gmail.com>
2014-03-30 22:10:16 +0800

    Setup data for the test via the admin site

--- mysite/fts/tests.py
+++ mysite/fts/tests.py
@@ -5,9 +5,29 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from collections import namedtuple
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+
+PollInfo = namedtuple('PollInfo', ['question', 'choices'])
+POLL1 = PollInfo(
+    question='How awesome is Test-Driven Development?',
+    choices=[
+        'Very awesome',
+        'Quite awesome',
+        'Moderately awesome',
+    ],
+)
+POLL2 = PollInfo(
+    question="Which workshop treat do you prefer?",
+    choices=[
+        'Beer',
+        'Pizza',
+        'The Acquisition of Knowledge',
+    ],
+)
 
 
 class PollsTest(LiveServerTestCase):
@@ -85,11 +105,56 @@ class PollsTest(LiveServerTestCase):
 
         # Satisfied
 
+    def _setup_polls_via_admin(self):
+        # Logs into the admin page
+        self.browser.get(self.live_server_url + '/admin/')
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('admin')
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('adm1n')
+        password_field.send_keys(Keys.RETURN)
+
+        # Enters a number of polls
+        for poll_info in [POLL1, POLL2]:
+            # Follows the link to the polls app, and adds a new poll
+            polls_links = self.browser.find_elements_by_link_text('Polls')
+            polls_links[1].click()
+            add_poll_link = self.browser.find_element_by_link_text('Add poll')
+            add_poll_link.click()
+
+            # Enters question, uses today and now to set publish date
+            question_field = self.browser.find_element_by_name('question')
+            question_field.send_keys(poll_info.question)
+            self.browser.find_element_by_link_text('Today').click()
+            self.browser.find_element_by_link_text('Now').click()
+
+            for i, choice_text in enumerate(poll_info.choices):
+                choice_field = self.browser.find_element_by_name('choice_set-%d-choice' % i)
+                choice_field.send_keys(choice_text)
+
+            # Clicks the save button to save new poll
+            save_button = self.browser.find_element_by_css_selector("input[value='Save']")
+            save_button.click()
+
+            # Is returned to the 'Polls' listing, sees the new poll,
+            # listed as a clickable link by its name
+            new_poll_links = self.browser.find_elements_by_link_text(poll_info.question)
+            self.assertEqual(len(new_poll_links), 1)
+
+            # Goes back to the root of the admin site
+            self.browser.get(self.live_server_url + '/admin/')
+
+        # Logs out of the admin page
+        log_out_link = self.browser.find_element_by_link_text('Log out')
+        log_out_link.click()
+
+
     def test_voting_on_a_new_poll(self):
-        pass
         # First, the administrator logs into the admin site and
         # creates a couple of new polls, and their response choices
+        self._setup_polls_via_admin()
 
+        self.fail('TODO')
         # Now the regular user goes to the homepage of the site,
         # sees a list of polls
 
