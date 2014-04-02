a91b0019d2bd25b6625f7004511a9b7539f26099
Tolbkni Kao <tolbkni@gmail.com>
2014-04-02 23:43:54 +0800

    Add a test of form renders poll choices as radio inputs

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -9,6 +9,7 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.utils import timezone
 from polls.models import Choice, Poll
+from polls.forms import PollVoteForm
 
 
 class PollModelTest(TestCase):
@@ -141,3 +142,34 @@ class SinglePollViewTest(TestCase):
 
         # Check our 'no votes yet' message appears
         self.assertIn('No-one has voted on this poll yet', content)
+
+
+class PollsVoteFormTest(TestCase):
+
+    def test_form_renders_poll_choices_as_radio_inputs(self):
+        # Setup a poll with a couple of choices
+        poll1 = Poll(question='6 times 7', pub_date=timezone.now())
+        poll1.save()
+        choice1 = Choice(poll=poll1, choice='42', votes=0)
+        choice1.save()
+        choice2 = Choice(poll=poll1, choice='The Ultimate Answer', votes=0)
+        choice2.save()
+
+        # Setup another poll to make sure we only see the right choices
+        poll2 = Poll(question='time', pub_date=timezone.now())
+        poll2.save()
+        choice3 = Choice(poll=poll2, choice='PM', votes=0)
+        choice3.save()
+
+        # build a voting form for poll1
+        form = PollVoteForm(poll=poll1)
+
+        self.assertEqual([key for key in form.fields.keys()], ['vote'])
+
+        self.assertEqual(form.fields['vote'].choices, [
+            (choice1.id, choice1.choice),
+            (choice2.id, choice2.choice),
+        ])
+
+        # Check it uses radio inputs to render
+        self.assertIn('type="radio"', form.as_p())
